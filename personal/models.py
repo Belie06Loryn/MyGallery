42fd9dd7f639ff6789591c394e830feed7fa4810
Belie06Loryn <maniralexie@gmail.com>
2019-10-09 16:34:27 +0200

    creating project and application

--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+import datetime as dt
+class Category(models.Model):
+    cate = models.CharField(max_length =30)
 
-# Create your models here.
+    def save_cate(self):
+        self.save()
+
+    def dele_cate(self):
+        self.delete() 
+
+    def __str__(self):
+        return self.cate       
+
+class Location(models.Model):
+    location = models.CharField(max_length =30)
+
+    def save_loca(self):
+        self.save()
+
+    def dele_loca(self):
+        self.delete()    
+
+    def __str__(self):
+        return self.location
+
+class Photos(models.Model):
+    image = models.ImageField(upload_to = 'photos/',null=True)
+    name = models.CharField(max_length =40)
+    descri = models.TextField(max_length =6000)
+    cate = models.ForeignKey(Category)
+    loca = models.ForeignKey(Location)
+    pub_date = models.DateTimeField(auto_now_add=True) 
+    
+    def save_pic(self):
+        self.save()
+                      
+    @classmethod
+    def search_by_cate(cls,search_term):
+        categories = cls.objects.filter(category__icontains=search_term)
+        return categories  
+
+    def dele_pic(self):
+        self.delete() 
+
+    @classmethod
+    def filter_loca(cls,location):
+        imaje = cls.objects.filter(location=location)
+        return imaje
+
+    @classmethod
+    def update_pic(cls,id):
+        imaje = cls.objects.filter(id=id).update(id=id)
+        return imaje
+
+    def __str__(self):
+        return self.name                 
--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Image,Category,Location
 
 # Create your views here.
+
+def welcome(request):
+    imaje = Image.objects.all()
+    return render(request, 'all-photos/index.html',{"imaje":imaje,})
+
+
+def search_results(request):
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get("category")
+        searched_categories = Image.search_by_category(search_term)
+        
+        message = f"{search_term}"
+        
+        return render(request, 'all-photos/search.html',{"message":message,"categories":searched_categories})
+    
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-photos/search.html',{"message":message})
+
+def photo(request,photo_id):
+    try:
+        photo = photo.objects.get(id = photo_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-news/photo.html", {"photo":photo})
