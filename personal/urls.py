da01b620ccd7c67e5c75dd765e1ebf78b44675f6
Belie06Loryn <maniralexie@gmail.com>
2019-10-09 17:32:35 +0200

    migration

--- personal/urls.py
+++ personal/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name='welcome'),
+    url('^$',views.page,name='page'),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^personal/(\d+)',views.photo,name ='photo'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- personal/views.py
+++ personal/views.py
@@ -1,18 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Image,Category,Location
+from .models import Photos,Category,Location
 
-# Create your views here.
-
-def welcome(request):
-    imaje = Image.objects.all()
+def page(request):
+    imaje = Photos.objects.all()
     return render(request, 'all-photos/index.html',{"imaje":imaje,})
 
 
 def search_results(request):
     if 'category' in request.GET and request.GET['category']:
         search_term = request.GET.get("category")
-        searched_categories = Image.search_by_category(search_term)
+        searched_categories = Photos.search_by_category(search_term)
         
         message = f"{search_term}"
         
@@ -27,4 +25,4 @@ def photo(request,photo_id):
         photo = photo.objects.get(id = photo_id)
     except DoesNotExist:
         raise Http404()
-    return render(request,"all-news/photo.html", {"photo":photo})
+    return render(request,"all-photos/personal.html", {"photo":photo})
